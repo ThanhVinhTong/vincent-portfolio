462ddc9bd444f2aff674a599b87c8c009a038866
ThanhVinhTong <vinhtongthanh57@gmail.com>
2026-02-20 12:52:43 +0800

    feat: define portfolio structures

---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,30 @@
+const navLinks = [
+  {href: "#about", label: "About"},
+  {href: "#projects", label: "Projects"},
+  {href: "#experience", label: "Experience"},
+  {href: "#testimonials", label: "Testimonials"},
+]
+
+
+export const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <a>
+          PM<span>.</span>
+        </a>
+
+        {/* Desktop Navigation */}
+        <div>
+          <div>
+            {navLinks.map((link, index) => (
+              <a key={index} href={link.href}>
+                {link.label}
+              </a>
+            ))}
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
